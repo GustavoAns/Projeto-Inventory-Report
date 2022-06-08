fd4bf87f37d6b3df94b0e7e81751e6d53db9ae81
GustavoAns <gugasato9@live.com>
2022-06-08 16:06:19 -0300

    Add req 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        "Cadeira",
+        "Casa do Código",
+        "01/01/2020",
+        "01/01/2021",
+        "123456789",
+        "na cozinha",
+    )
+
+    message = "O produto Cadeira fabricado em 01/01/2020 por Casa do Código " \
+        "com validade até 01/01/2021 precisa ser armazenado na cozinha."
+
+    assert str(new_product) == message
