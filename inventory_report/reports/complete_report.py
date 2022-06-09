4d43af7880c214bb361e3dea18e778b653de734d
GustavoAns <gugasato9@live.com>
2022-06-09 16:26:26 -0300

    Add req 4

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,13 +1,24 @@
 from inventory_report.reports.simple_report import SimpleReport
-# from collections import Counter
+from collections import Counter
 
 
 class CompleteReport(SimpleReport):
     @classmethod
     def generate(cls, products_list):
         log = super().generate(products_list)
-        print(log)
+        empresas = []
+        empresas_quant = ""
+
+        for product in products_list:
+            empresas.append(product["nome_da_empresa"])
+
+        contador = Counter(empresas).most_common()
+
+        for empresa, quantidade in contador:
+            empresas_quant += f"- {empresa}: {quantidade}\n"
+
         return (
             f"{log}\n"
-            f"Coisas legais"
+            f"Produtos estocados por empresa:\n"
+            f"{empresas_quant}"
         )
